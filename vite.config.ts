8f6dee5f9bed1f508e51eb37c22c2c16ccc7d400
hendrytechfloripa <jony.vp@hotmail.com>
2026-02-25 23:46:47 -0300

    fix: vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+
+    // 🔥 Resolve alias @
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+    // 🔥 Resolve GitHub Pages
+    base: command === "build" ? "/sitehendrytech/" : "/",
+
+    build: {
+      outDir: "dist",
     },
-  },
-}));
+  };
+});
